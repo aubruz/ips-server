5491aa6692272da7ca6a592f83f8719eba0fc23d
Aubruz <aubruz@hotmail.com>
2018-11-02 18:21:05 +0100

    Fix namespaces

--- app/Http/Resources/BuildingCollection.php
+++ app/Http/Resources/BuildingCollection.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class Points extends ResourceCollection
+class BuildingCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
@@ -12,14 +12,12 @@ class Points extends ResourceCollection
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
-    public function toArray($point)
+    public function toArray($building)
     {
         return [
-            'id'        => $point->encoded_id,
-            'name'      => $point->name,
-            'location'  => $point->location,
-            'x'         => $point->x,
-            'y'         => $point->y,
+            'id'        => $building->encoded_id,
+            'name'      => $building->name,
+            'address'   => $building->address,
         ];
     }
 }
