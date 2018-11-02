87aba01fc6be5c0fb5c3edb95ac2c940865c09c4
Aubruz <aubruz@hotmail.com>
2018-11-02 13:03:06 +0100

    Add resources instead of transformers

---
+++ app/Http/Resources/Floors.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class Floors extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($floor)
+    {
+        return [
+            'id'        => $floor->encoded_id,
+            'name'      => $floor->name,
+            'blueprint' => $floor->blueprint,
+            'width'     => $floor->width,
+            'height'    => $floor->height,
+        ];
+    }
+}
---
+++ app/Http/Resources/Points.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class Points extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($point)
+    {
+        return [
+            'id'        => $point->encoded_id,
+            'name'      => $point->name,
+            'location'  => $point->location,
+            'x'         => $point->x,
+            'y'         => $point->y,
+        ];
+    }
+}
